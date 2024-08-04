c397a6100f4067c814dbeda46692cee3cd29b927
Mariia <90716920+Mariia-Petlovana-91@users.noreply.github.com>
2024-08-04 22:32:43 +0200

    start_work

--- vite.config.js
+++ vite.config.js
@@ -11,20 +11,21 @@ export default defineConfig(({ command }) => {
     root: 'src',
     build: {
       sourcemap: true,
-
       rollupOptions: {
         input: glob.sync('./src/*.html'),
-        output: {
-          manualChunks(id) {
-            if (id.includes('node_modules')) {
-              return 'vendor';
-            }
-          },
-          entryFileNames: 'commonHelpers.js',
-        },
       },
       outDir: '../dist',
     },
-    plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
+    plugins: [
+      injectHTML(),
+      FullReload(['./src/**/**.html'])
+    ],
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "./css/main.scss";`
+        }
+      }
+    }
   };
 });
