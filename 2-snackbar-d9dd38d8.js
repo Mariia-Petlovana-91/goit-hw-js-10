ad79f889995fc459856df17c099c0a1d4fc673dc
Mariia-Petlovana-91 <90716920+Mariia-Petlovana-91@users.noreply.github.com>
2024-08-10 05:07:43 +0000

    Deploying to gh-pages from @ Mariia-Petlovana-91/goit-hw-js-10@0d7c75c5ce1331a94fb20bd15094abd29b2fd7a7 🚀

--- 2-snackbar-d9dd38d8.js
+++
@@ -1,2 +0,0 @@
-import"./iziToast.min-98ae614a.js";/* empty css              */const e={form:document.querySelector(".form")};e.form.addEventListener("input",n);e.form.addEventListener("change",o);e.form.addEventListener("submit",r);function n(t){}function o(t){}function r(t){}
-//# sourceMappingURL=2-snackbar-d9dd38d8.js.map
