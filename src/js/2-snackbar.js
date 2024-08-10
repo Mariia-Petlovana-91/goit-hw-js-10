0d7c75c5ce1331a94fb20bd15094abd29b2fd7a7
Mariia <90716920+Mariia-Petlovana-91@users.noreply.github.com>
2024-08-10 07:07:12 +0200

    build timer

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,52 +1,52 @@
-import iziToast from 'izitoast';
-import 'izitoast/dist/css/iziToast.min.css';
+// import iziToast from 'izitoast';
+// import 'izitoast/dist/css/iziToast.min.css';
 
-const refs = {
-	form: document.querySelector('.form'),
-	delay: document.querySelector('[name="delay"]'),
-	statusRatio: document.querySelector('.form__inputContainer'),
+// const refs = {
+// 	form: document.querySelector('.form'),
+// 	delay: document.querySelector('[name="delay"]'),
+// 	statusRatio: document.querySelector('.form__inputContainer'),
 
-}
+// }
 
- refs.form.addEventListener('submit', createPromise);
- refs.delay.addEventListener('input', result);
- refs.statusRatio.addEventListener('change', result);
+//  refs.form.addEventListener('submit', createPromise);
+//  refs.delay.addEventListener('input', result);
+//  refs.statusRatio.addEventListener('change', result);
 
-function result (e){
-  e.target.value;
-}
-
-
+// function result (e){
+//   e.target.value;
+// }
 
 
 
-function createPromise(e){
-	e.preventDefault();
 
-	const promise = new Promise((resolve, reject) => {
-	     setTimeout((delay) => {
-		 if (result===) {
-		   resolve(iziToast.show({
-		     backgroundColor: 'green',
-		     messageColor: 'white',
-		     message: `✅ Fulfilled promise in ${delay} ms`,
-		     position: 'topRight',
-		     close: false,
-		   }));
-		 } else {
-		   reject(iziToast.show({
-		     backgroundColor: 'red',
-		     messageColor: 'white',
-		     message: `❌ Rejected promise in ${delay} ms`,
-		     position: 'topRight',
-		     close: false,
-		   }));
-		 }
-	     });
-	   });
 
-	   console.log(promise);
-}
+// function createPromise(e){
+// 	e.preventDefault();
+
+// 	const promise = new Promise((resolve, reject) => {
+// 	     setTimeout((delay) => {
+// 		 if () {
+// 		   resolve(iziToast.show({
+// 		     backgroundColor: 'green',
+// 		     messageColor: 'white',
+// 		     message: `✅ Fulfilled promise in ${delay} ms`,
+// 		     position: 'topRight',
+// 		     close: false,
+// 		   }));
+// 		 } else {
+// 		   reject(iziToast.show({
+// 		     backgroundColor: 'red',
+// 		     messageColor: 'white',
+// 		     message: `❌ Rejected promise in ${delay} ms`,
+// 		     position: 'topRight',
+// 		     close: false,
+// 		   }));
+// 		 }
+// 	     });
+// 	   });
+
+// 	   console.log(promise);
+// }
 
 
 
